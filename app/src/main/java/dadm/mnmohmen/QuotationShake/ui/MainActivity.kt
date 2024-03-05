c621339580108e2f97aba1984bf202cf5bf17fed
moha-drk <133339852+moha-drk@users.noreply.github.com>
2024-03-05 15:57:44 +0100

    fino señores

--- app/src/main/java/dadm/mnmohmen/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/mnmohmen/QuotationShake/ui/MainActivity.kt
@@ -3,6 +3,11 @@ package dadm.mnmohmen.QuotationShake.ui
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dadm.mnmohmen.QuotationShake.R
 import dadm.mnmohmen.QuotationShake.databinding.ActivityMainBinding
@@ -14,22 +19,9 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.yepa.text = "Hello Wold ma G"
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.newQF -> {
-                    true
-                }
-                R.id.favsF -> {
-                    true
-                }
-                R.id.settingsF -> {
-                    true
-                }
-                else -> false
-            }
-        }
+        val navController = findNavController(R.id.fragment_container)
+        binding.bottomNavigation.setupWithNavController(navController)
+        
     }
 
 }
